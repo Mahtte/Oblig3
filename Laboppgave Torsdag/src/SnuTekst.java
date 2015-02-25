4c72db19a7ea3369f76f836afb52eed3569f90f3
Mahtte <530521@student.hin.no>
2015-02-25 22:35:17 +0100

    V0.1

--- Laboppgave Torsdag/src/SnuTekst.java
+++ Laboppgave Torsdag/src/SnuTekst.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
 
 public class SnuTekst {
 	
 	public static void main(String[] args) {
-		baklengs("hei");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Skriv inn en tekststreng: ");
+		String tekst = input.nextLine();
+		System.out.println(tekst + " baklengs blir: ");
+		baklengs(tekst);
+		input.close();
 	}
 	
 	public static void baklengs(String tekst) {
 		if (tekst.length() <= 1)		// Base case
 			System.out.print(tekst.charAt(tekst.length() - 1));
-		else 
+		else {
+			System.out.print(tekst.charAt(tekst.length() - 1));
 			baklengs(tekst.substring(0, tekst.length() - 1));
+		}
 	}
 }
