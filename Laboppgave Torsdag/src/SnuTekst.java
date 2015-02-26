fcf22c0a9d3cc250d8561ac0212586ad6e819992
Mahtte <530521@student.hin.no>
2015-02-26 20:24:51 +0100

    SnuTekst versjon 0.2. Lagt til en funksjonalitet som teller antall tegn.

--- Laboppgave Torsdag/src/SnuTekst.java
+++ Laboppgave Torsdag/src/SnuTekst.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
 
 public class SnuTekst {
 	
 	public static void main(String[] args) {
-		baklengs("hei");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Skriv inn en tekststreng: ");
+		String tekst = input.nextLine();
+		System.out.println(tekst + " baklengs blir: ");
+		baklengs(tekst);
+		input.close();
 	}
 	
+	static int antallTegn = 0;
+	
 	public static void baklengs(String tekst) {
-		if (tekst.length() <= 1)		// Base case
+		antallTegn++;
+		if (tekst.length() <= 1) {		// Base case
 			System.out.print(tekst.charAt(tekst.length() - 1));
-		else 
+			System.out.printf("%n" + "Antall tegn: " + antallTegn);
+		}
+		else {
+			System.out.print(tekst.charAt(tekst.length() - 1));
 			baklengs(tekst.substring(0, tekst.length() - 1));
+			antallTegn++;
+		}
 	}
 }
